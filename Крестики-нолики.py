bc99e15d7254e32a40d9eeacc74959e6f04533ee
daSchmdt <108695123+daSchmdt@users.noreply.github.com>
2022-07-04 22:39:50 +0300

    Add files via upload

---
+++ Крестики-нолики.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def greet():
+    print(" Добро пожаловать в игру 'Крестики-нолики!' ")
+    print("--------------------------------------------")
+    print(" Игровое поле 3х3 обозначено цифрами 0, 1, 2")
+    print("--------------------------------------------")
+    print("   Первая цифра - координата по вертикали   ")
+    print("  Вторая цифра - координата по Горизонтали  ")
+    print("--------------------------------------------")
+    print("   Вводить координаты нужно через пробел    ")
+
+field = [[" "] * 3 for i in range(3) ]
+
+print("Введите две цифры через пробел чтобы начать")
+
+def show():
+    print()
+    print("    |  0 | 1 | 2 | ")
+    print("  ----------------")
+    for i, row in enumerate(field):
+        row_str = f"  {i} |  {' | '.join(row)} | "
+        print(row_str)
+        print("  ----------------")
+    print()
+
+
+def ask():
+    while True:
+        cords = input("        Ваш ход: ").split()
+
+        if len(cords) != 2:
+            print(" Введите две координаты! ")
+            continue
+
+        x, y = cords
+
+        if not(x.isdigit()) or not(y.isdigit()):
+            print(" Введите числа! ")
+            continue
+
+        x, y = int(x), int(y)
+
+        if 0 > x or x > 2 or 0 > y or y > 2:
+            print(" Координаты вне диапазона! ")
+            continue
+        if field[x][y] != " ":
+            print( " Клетка занята! ")
+            continue
+
+        return x, y
+ask()
+
+def check_win():
+    win_cord = (((0, 0), (0, 1), (0, 2)), ((1, 0), (1, 1), (1, 2)), ((2, 0), (2, 1), (2, 2)),
+                ((0, 2), (1, 1), (2, 0)), ((0, 0), (1, 1), (2, 2)), ((0, 0), (1, 0), (2, 0)),
+                ((0, 1), (1, 1), (2, 1)), ((0, 2), (1, 2), (2, 2)))
+    for cord in win_cord:
+        symbols = []
+        for c in cord:
+            symbols.append(field[c[0]][c[1]])
+        if symbols == ["X", "X", "X"]:
+            print("Выиграл X!!!")
+            return True
+        if symbols == ["0", "0", "0"]:
+            print("Выиграл 0!!!")
+            return True
+    return False
+
+
+
+greet()
+field = [[" "] * 3 for i in range(3) ]
+count = 0
+while True:
+    count += 1
+    show()
+    if count % 2 == 1:
+        print(" Ходит крестик!")
+    else:
+        print(" Ходит нолик!")
+    
+    x, y = ask()
+    
+    if count % 2 == 1:
+        field[x][y] = "X"
+    else:
+        field[x][y] = "0"
+    
+    if check_win():
+        break
+    
+    if count == 9:
+        print(" Ничья!")
+        break
+
+
+# In[ ]:
+
+
+
+
